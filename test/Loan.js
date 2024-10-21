1f761cfc71e6e00ebd47f8a27227c6be994e5463
chenqilin70 <kylinchen85@foxmail.com>
2024-10-21 21:02:50 +0800

    UPDATE

--- test/Loan.js
+++ test/Loan.js
@@ -6,15 +6,13 @@ describe("贷款合约",async function(){
     let loan;
 
     before(async function () {
-        console.log("11111111")
         const Loan = await ethers.getContractFactory("Loan");
         loan = await Loan.deploy();
-        console.log("22222222")
     });
 
     it("新增筹资项目", async function () {
         var p = await loan.createProject(
-            100,40000,10,1729568530000,1
+            100,40000,10,1732193392,1
         );
         console.log(p);
         var bs = await loan.bills(0,0)
